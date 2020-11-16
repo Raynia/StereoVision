79c5bde215289713564ce1049d1ff9ab4e8efa96
Raynia <46100945+Raynia@users.noreply.github.com>
2020-11-16 16:21:57 +0900

    Update admin.py

--- stereovision/admin.py
+++ stereovision/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CameraInfo, Userdata, TargetImage, CameraList
+from .models import CameraInfo, Userdata, TargetImage, CameraList, PreviewCamera
 # Register your models here.
 
 admin.site.register(CameraInfo)
 admin.site.register(Userdata)
 admin.site.register(TargetImage)
 admin.site.register(CameraList)
+admin.site.register(PreviewCamera)
